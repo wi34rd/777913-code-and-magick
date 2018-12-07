beecf27d590adee18003bb7407d44a5744646b37
Keks <keks@htmlacademy.ru>
2018-12-07 22:55:12 +0300

    :heavy_check_mark: Сборка #3

--- 3/js/setup.js
+++ 3/js/setup.js
@@ -4,51 +4,54 @@ var setupSimular = setup.querySelector('.setup-similar');
 setup.classList.remove('hidden');
 setupSimular.classList.remove('hidden');
 
-// Функция отрисовки волшебников в меню выбора
 var template = document.querySelector('#similar-wizard-template');
 var setupSimilarList = document.querySelector('.setup-similar-list');
 
 var PERSON_NAME = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 var PERSON_SURNAME = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
-var coatColor = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+var COAT_COLOR = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+var EYES_COLOR = ['black', 'red', 'blue', 'yellow', 'green'];
 
-var copyWizards = function () {
-//   var PERSON_NAME = ["Иван", "Хуан Себастьян", "Мария", "Кристоф", "Виктор", "Юлия", "Люпита", "Вашингтон"];
+// Функция рандомных чисел для меняющихся предметов
+var getRandomElement = function (array) {
+  var index = Math.round(Math.random() * (array.length - 1));
+  return array[index];
+};
 
-  for (var i = 0; i < 4; i++) {
+// var WIZARDS = [];
+// var WIZARDS_COUNT = 4;
+
+var createWizards = function () {
+  var wizards = [];
+  var WIZARDS_COUNT = 4;
+
+  for (var i = 0; i < WIZARDS_COUNT; i++) {
+    var newWizard = {
+      name: getRandomElement(PERSON_NAME),
+      surname: getRandomElement(PERSON_SURNAME),
+      coat: getRandomElement(COAT_COLOR),
+      eyes: getRandomElement(EYES_COLOR)
+    };
+    wizards.push(newWizard);
+
+    return wizards;
+  }
+};
+
+var WIZARDS = createWizards();
+
+// Функция отрисовки волшебников в меню выбора
+var copyWizards = function (WIZARDS) {
+  for (var i = 0; i < WIZARDS.length; i++) {
     var setupSimilarItem = template.content.querySelector('.setup-similar-item').cloneNode(true);
     var space = ' ';
 
-    setupSimilarItem.querySelector('.setup-similar-label').textContent = PERSON_NAME[i] + space + PERSON_SURNAME[i];
-    setupSimilarItem.querySelector('.wizard-coat').fill = coatColor[i];
+    setupSimilarItem.querySelector('.setup-similar-label').textContent = WIZARDS[i].name + space + WIZARDS[i].surname;
+    setupSimilarItem.querySelector('.wizard-coat').style.fill = WIZARDS[i].coat;
+    setupSimilarItem.querySelector('.wizard-eyes').style.fill = WIZARDS[i].eyes;
 
     setupSimilarList.appendChild(setupSimilarItem);
   }
 };
 
-copyWizards();
-
-// var generateData = function (PERSON_NAME, PERSON_SURNAME) {
-//   for (var i = 0; i <= PERSON_NAME.length - 5; i++) {
-//     console.log(PERSON_NAME[i]);
-//   }
-// };
-
-// generateData();
-
-// var coatColor = {
-//   coat: ["rgb(101, 137, 164)", 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)']
-// };
-
-// var eyesColor = {
-//   eyes: ['black', 'red', 'blue', 'yellow', 'green']
-// };
-
-// var getRandomValue = functi on (eyesColor.eyes) {
-//   for (var i = 0; i <= eyesColor.eyes.length - 1; i++) {
-//     Math.random(eyesColor.eyes.length-0);
-//   }
-// }
-
-console.log(document.querySelector('.setup-similar-list'));
-console.log(document.querySelector('#similar-wizard-template'));
+copyWizards(WIZARDS);
